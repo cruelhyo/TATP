9dd2f5f668a1a60caae8dd369b1a3eee9afc70cc
cruelhyo <cruelhyo@naver.com>
2017-08-24 17:08:21 +0900

    로그찍는부분 살짝 수정

--- src/main/java/take/a/talent/member/service/UserAuthenticationService.java
+++ src/main/java/take/a/talent/member/service/UserAuthenticationService.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import take.a.talent.member.vo.MemberDetailsVO;
+import take.a.talent.member.vo.MemberDetailsVo;
 
 public class UserAuthenticationService implements UserDetailsService
 {
@@ -61,13 +61,13 @@ public class UserAuthenticationService implements UserDetailsService
 		// 분기점을 두어 VO객체를 생성할 때 enabled에 false를 넣고 생성 후 리턴
 		if (user.get("authority") == "ROLE_WITHDRAWAL_MEM" || user.get("authority") == "ROLE_RESTRICTION")
 		{
-			return new MemberDetailsVO(user.get("member_id").toString(), user.get("member_pw").toString(), false, false,
+			return new MemberDetailsVo(user.get("member_id").toString(), user.get("member_pw").toString(), false, false,
 					false, false, gas);
 		}
 
 		// ROLE_WITHDRAWAL_MEM, ROLE_RESTRICTION 권한이 아닌 나머지 권한을 가진 유저는 전부 true로 설정, 맵핑한
 		// 값을 넣은 후 리턴한다
-		return new MemberDetailsVO(user.get("member_id").toString(), user.get("member_pw").toString(), true, true, true,
+		return new MemberDetailsVo(user.get("member_id").toString(), user.get("member_pw").toString(), true, true, true,
 				true, gas);
 
 	}

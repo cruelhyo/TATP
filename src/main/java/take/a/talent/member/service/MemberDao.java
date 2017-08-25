aca4b07a3fa819023af35cc56e87bafcb44a416c
tkdgns712 <tkdgns1317@naver.com>
2017-08-25 17:20:07 +0900

    password 암호화추가 작업 완료
    작성자: 김상훈

--- src/main/java/take/a/talent/member/service/MemberDao.java
+++ src/main/java/take/a/talent/member/service/MemberDao.java
@@ -4,7 +4,10 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
+
+import com.mysql.fabric.xmlrpc.base.Member;
 
 import take.a.talent.member.controller.MemberController;
 import take.a.talent.member.vo.MemberVo;
@@ -13,6 +16,9 @@ import take.a.talent.member.vo.MemberVo;
 
 public class MemberDao implements MemberDaoInterface{
 		
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
 	//sql문 작동시킬 sqlsession의 자동 객체화 
 	@Autowired
 	 private SqlSessionTemplate sqlSessionTemplate;
@@ -23,6 +29,12 @@ public class MemberDao implements MemberDaoInterface{
 	@Override
 	public int insertMember(MemberVo memberVo) {
 		logger.info("DAO insertMember 호출");
+				
+		String encryptPassword = passwordEncoder.encode(memberVo.getMember_password());
+		
+		memberVo.setMember_password(encryptPassword);
+		
+		
 		int row = sqlSessionTemplate.insert("take.a.talent.member.service.MemberMapper.insertMember", memberVo);
 		return row;
 	}
